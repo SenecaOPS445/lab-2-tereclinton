799862227d1d85e9afee91fdff7d13ae7b72499c
Oghenetega <otere@myseneca.ca>
2024-06-08 14:11:19 -0400

    added lab2e.py

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Oghenetega Tere
+#Author ID: 119911220
+#Date Created: 2024/06/18
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
